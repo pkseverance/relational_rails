3b1898f140f6801d71eaf2c14e2b54e56eaf90e8
Patricia Severance <patriciakseverance@gmail.com>
2022-12-01 13:31:36 -0700

    Add routes for players & games

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/players', to: 'players#index'
+  get '/players/:id', to: 'players#show'
+
+  get '/games', to: 'games#index'
+  get '/games/:id', to: 'games#show'
 end
